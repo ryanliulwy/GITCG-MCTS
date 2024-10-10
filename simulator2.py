e257bc4f9ae309ad24d258789ca897d0ff820c83
ryanliulwy <ryl001@ucsd.edu>
2024-10-09 22:40:52 -0700

    manually implemented a 'random' agent that always rerolls all of its starting dice and always makes smart selection for dice costs

--- simulator2.py
+++ simulator2.py
@@ -9,20 +9,21 @@ for _ in range(1):
     pid = Pid.P1
 
     while not done:
+        if (turn == 2): pid = Pid.P2
+        else: pid = Pid.P1
         act_gen: ActionGenerator = game_state.action_generator(pid)
         while not act_gen.filled():
             choices = act_gen.choices()
+            print(choices)
             if (type(choices) == tuple) :
                 choice = random.choice(choices)
             elif (type(choices) == ActualDice):
-                choice = random.choice(list(choices._dice.keys()))
+                choice = act_gen.dice_available()
             elif (type(choices) == AbstractDice):
-                choice = random.choice(list(choices.keys()))
+                choice = act_gen.dice_available().smart_selection(choices)
             elif (type(choices) == Cards):
                 choice = choices.pick_random(random.randint(0,5))[0]
             act_gen = act_gen.choose(choice)
         action = act_gen.generate_action()
         game_state, encoded_state, reward, turn, done = env.step(action)
-        if (pid == Pid.P1): pid = Pid.P2
-        else: pid = Pid.P1
         if (game_state.game_end()): print (game_state.get_winner())
