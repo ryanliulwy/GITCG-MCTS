2dd3fb4fcc8eee890bf60ce324217a1258304573
Lisa Liu <lisaliu041@gmail.com>
2025-02-20 10:12:40 -0800

    added small updates to gym + wandb logging

--- simulators/dottore_gym/envs/dottore_gym_env.py
+++ simulators/dottore_gym/envs/dottore_gym_env.py
@@ -1,10 +1,52 @@
 import gymnasium as gym
 from gymnasium import spaces
 import numpy as np
-from dgisim import LinearEnv  # Import your environment from dgisim
-
-
-# TODO missing implementation
+from dgisim import LinearEnv, PlayerAction, ActionGenerator 
+from typing import Optional
 
 class DottoreLinearGymEnv(gym.Env):
-    pass
+    def __init__(self):
+        self.env = LinearEnv()
+        self.render_mode = "human"
+        self.game_state = self.env.view()[0]
+
+        #   --- TODO can call action_space.sample() to randomly select an action. 
+        self.act_gen = self.game_state.action_generator(self.game_state.waiting_for())
+        self.action_space = spaces.Discrete(len(self.act_gen.choices()))
+
+        self.observation_space = spaces.Box(
+            low=0, high=1, shape=(len(self.env.view()[1]),), dtype=np.float32
+        )
+
+    def _get_obs(self):
+        """Returns the encoded state as an observation"""
+        return np.array(self.env.view()[1], dtype=np.float32)
+    
+    def _get_info(self):
+        """Returns additional info (empty for now)"""
+        return {}
+
+    def step(self, action):
+        print(self.act_gen.choices()[action])
+        print(type(self.act_gen.choices()[action]))
+        self.game_state, encoded_state, reward, turn, done = self.env.step(self.act_gen.choices()[action])
+    
+        observation = self._get_obs()
+        info = self._get_info()
+
+        self.act_gen = self.act_gen.choose(self.act_gen.choices()[action])
+        self.action_space = spaces.Discrete(len(self.act_gen.choices()))
+
+        return observation, reward, done, False, info
+    
+    def reset(self, seed: Optional[int] = None, options: Optional[dict] = None):
+        self.env.reset()
+        return self._get_obs(), self._get_info()
+    
+    def close():
+        pass
+
+    def render(self):
+        """Optional render function (modify if visual output is available)"""
+        if self.render_mode == "human":
+            print("Game State:", self.env.view()[0])
--- simulators/simulator_mcts_simple.py
+++ simulators/simulator_mcts_simple.py
@@ -8,15 +8,26 @@ from custom_agents.mcts_agent import MCTSAgent
 # from simulators.offline.mcts_offline_agent import OfflineAgent
 import random, time
 from offline.deck_default import deck1 as deckDefault
+import wandb
 
 agRand = RandomAgent()
 agMCTS = MCTSAgent()
 
-start = time.time()
+wandb.init(project="dgisim-regular-mcts", name=f"random_vs_mcts_50")
 
 p1_wins = 0
 p2_wins = 0
-total_games = 1
+total_games = 50
+
+action_counts = {} # action tracker
+
+wandb.config.update({
+    "total_games": total_games,
+    "p1_agent": "RandomAgent",
+    "p2_agent": "MCTSAgent"
+})
+
+start = time.time()
 for i in range(total_games):
     game_state = GameState.from_decks(
         mode=dg.AllOmniMode(),
@@ -31,13 +42,29 @@ for i in range(total_games):
             current_player = game_state.waiting_for()
             action = agRand.choose_action(history, current_player)
             game_state = game_state.action_step(current_player, action)
-            print(current_player, action)
+            # print(current_player, action)
+            # tracking
+            action_type = action.__class__.__name__  # e.g., "CardAction", "SkillAction"
+            key = f"{current_player} {action_type}"
+            if key in action_counts:
+                action_counts[key] += 1  
+            else:
+                action_counts[key] = 1
         elif game_state.waiting_for() is Pid.P2: # MCTS goes second
             current_player = game_state.waiting_for()
             action = agMCTS.choose_action(history, current_player)
             game_state = game_state.action_step(current_player, action)
-            print(current_player, action)
+            # print(current_player, action)
+            # tracking
+            action_type = action.__class__.__name__  # e.g., "CardAction", "SkillAction"
+            key = f"{current_player} {action_type}"
+            if key in action_counts:
+                action_counts[key] += 1  
+            else:
+                action_counts[key] = 1
+            
         history.append(game_state)
+
     print("Game #%d complete" % i)
     print(game_state.get_winner())
     if game_state.get_winner() == Pid.P1:
@@ -45,8 +72,24 @@ for i in range(total_games):
     elif game_state.get_winner() == Pid.P2:
         p2_wins += 1
 
+
 end = time.time()
-print(end - start)
+print(end - start, " seconds taken.")
 print("P1 wins:", p1_wins)
 print("P2 wins:", p2_wins)
 print("Total games:", total_games)
+
+
+wandb.log({
+    "total_duration": end - start,
+    "final_p1_wins": p1_wins,
+    "final_p2_wins": p2_wins,
+    "final_p1_win_rate": p1_wins / total_games,
+    "final_p2_win_rate": p2_wins / total_games
+})
+
+wandb.log(action_counts)
+for action, count in action_counts.items():
+    print(f"{action}: {count} times")
+
+wandb.finish()
